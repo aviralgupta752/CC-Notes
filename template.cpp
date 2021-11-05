a8881c77c8637d50249aa75efc24c6acd70f26c7
Aviral Gupta <guptaavi352@gmail.com>
2021-11-05 23:55:05 +0530

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int32_t main(){
+    int t;
+    cin>>t;
+
+}
