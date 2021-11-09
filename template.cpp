205ca7f57ef09c87c6b3fa9852e145ded8d0d3d4
Aviral Gupta <guptaavi352@gmail.com>
2021-11-09 17:55:12 +0530

    Added submask enumeration to notes

--- template.cpp
+++ template.cpp
@@ -3,7 +3,10 @@ using namespace std;
 #define int long long
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 int32_t main(){
+    fastio;
     int t;
     cin>>t;
+    while(t--){
 
+    }
 }
